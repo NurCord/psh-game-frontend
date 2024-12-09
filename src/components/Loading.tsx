8c4ab1e3ee2edc1bd57e2373edfa4928c7b38ccf
nurcord <nunyypin@gmail.com>
2024-12-09 14:33:55 -0300

    feat: add loading component and refactor data fetching logic

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+    return <div className="flex flex-col justify-center items-center min-h-screen">Loading...</div>;
+};
+
+export default Loading;
